c63bbc841e0b8426da1d2eb3275db530db1f0715
Bookrage001 <Bookrage001@gmail.com>
2021-02-13 17:04:10 +1100

    WIP: update to angular 11

--- src/app/poducts/life-mystree-game/life-mystree-game.component.spec.ts
+++ src/app/poducts/life-mystree-game/life-mystree-game.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LifeMystreeGameComponent } from './life-mystree-game.component';
 
@@ -6,7 +6,7 @@ describe('LifeMystreeGameComponent', () => {
   let component: LifeMystreeGameComponent;
   let fixture: ComponentFixture<LifeMystreeGameComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LifeMystreeGameComponent ]
     })
--- src/app/poducts/poducts.component.spec.ts
+++ src/app/poducts/poducts.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PoductsComponent } from './poducts.component';
 
@@ -6,7 +6,7 @@ describe('PoductsComponent', () => {
   let component: PoductsComponent;
   let fixture: ComponentFixture<PoductsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PoductsComponent ]
     })
