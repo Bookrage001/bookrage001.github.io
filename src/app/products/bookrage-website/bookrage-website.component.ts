40a94b59b730887ef05b1423aaf726bb5f1bbc42
Caleb Ardern <Bookrage001@gmail.com>
2026-03-24 11:53:19 +1100

    feat(Projects): :sparkles: Introduce this website as a project as itself

---
+++ src/app/products/bookrage-website/bookrage-website.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookrage-website',
+  templateUrl: './bookrage-website.component.html',
+  styleUrls: ['./bookrage-website.component.scss'],
+  standalone: true
+})
+export class BookrageWebsiteComponent {
+}
