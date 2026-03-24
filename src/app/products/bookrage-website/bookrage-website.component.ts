94b60be3513e0dd9fde2f64d0ea5c4a36cf081b2
Caleb Ardern <Bookrage001@gmail.com>
2026-03-24 13:13:02 +1100

    fix: fix spelling and router link

--- src/app/products/bookrage-website/bookrage-website.component.ts
+++ src/app/products/bookrage-website/bookrage-website.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-bookrage-website',
   templateUrl: './bookrage-website.component.html',
   styleUrls: ['./bookrage-website.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [RouterModule]
 })
 export class BookrageWebsiteComponent {
 }
