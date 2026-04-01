5535f45b0f0a5e265c7065c09c8465da5fe02314
Caleb Ardern <Bookrage001@gmail.com>
2026-04-01 13:30:01 +1100

    feat: :sparkles: Add Volunteer app as a buisness case

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { AssertiveComponent } from './products/assertive/assertive.component';
 import { DistributedAvionicsComponent } from './products/distributed-avionics/distributed-avionics.component';
 import { LifeMystreeGameComponent } from './products/life-mystree-game/life-mystree-game.component';
 import { BookrageWebsiteComponent } from './products/bookrage-website/bookrage-website.component';
+import { VolunteerAppComponent } from './products/volunteer-app/volunteer-app.component';
 import { ProductsComponent } from './products/products.component';
 import { ToolsComponent } from './tools/tools.component';
 import { DocumentationComponent } from './documentation/documentation.component';
@@ -33,6 +34,7 @@ export const routes: Routes = [
     path: 'products', component: ProductsComponent, children: [
       { path: 'bookrage-website', component: BookrageWebsiteComponent },
       { path: 'life-mystree', component: LifeMystreeGameComponent },
+      { path: 'volunteer-app', component: VolunteerAppComponent },
       { path: 'assertive', component: AssertiveComponent },
       { path: 'distributed-avionics', component: DistributedAvionicsComponent }
     ]
